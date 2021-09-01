ab1f4c3ec5d55e51dda80b63fba9584680f9c6b5
kipmyk <kipmyk@gmail.com>
2021-09-01 11:40:50 +0300

    template parts

---
+++ templates/template.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Page Tempalate Name
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+  <div class="entry-content" style="margin: 200px !important;">
+
