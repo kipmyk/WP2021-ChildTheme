f414bd33b58fe8a6b81aab1f390ee482fe3e5f8e
kipmyk <kipmyk@gmail.com>
2021-09-02 18:35:47 +0300

    modified single.php

--- single.php
+++ single.php
@@ -17,56 +17,85 @@ get_header();
 <main id="site-content" role="main">
   <div class="entry-content" style="margin: 200px !important;">
 
-<?php 
+<?php while( the_flexible_field("flexible_content") ): ?>
+<?php if( get_row_layout() == "full_width_paragraph" ): ?>
+<section class="content-section">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="text-box">
+<h2 class="heading-genekor"><?php the_sub_field("paragraph_title"); ?></h2>
+<p class="genekor"><?php the_sub_field("paragraph_content"); ?></p>
+<div class="clearfix spacing-50"></div>
+<p class="genekor"><?php the_sub_field("subtitle"); ?></p>
+<ul id="list">
+<?php if(have_rows ('list')): while (have_rows ('list')): the_row(); ?>
+<li><?php the_sub_field("list_item"); ?></li>
+<?php endwhile; endif; ?>
+</ul>
+</div>
+<div class="clearfix spacing-50"></div>
+</div>
+<!-- end col-12 -->
+</div>
+<!-- end row -->
+</div>
+<!-- end container -->
+</section>
 
-//load field settings and values
-$name = get_field('name');
+<!-- end content-section -->
 
-	  echo '<h1>' . $name . '</h1>';
-	  
-$comment = get_field('comment');
-	  
-echo '<p>' . $comment . '</p>';	 
-	  
-//load image
-$image  = get_field('image');
-	  
-echo '<pre>';
-	  print_r($image);
-echo '<pre>';
+<?php elseif( get_row_layout() == "left_content_right_image_svg" ): ?>
+<section class="content-section" data-background="<?php the_sub_field('background_color'); ?>">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-lg-8">
+<div class="side-content left">
+<h2 class="heading-genekor"><?php the_sub_field('title'); ?></h2>
+<p class="genekor-text" style="font-size:17px;">
+<?php the_sub_field('content'); ?>
+</p>
+<div class="clearfix spacing-50"></div>
+</div>
+<!-- end side-content -->
+<div class="clearfix spacing-50"></div>
+</div>
+<!-- end col-8 -->
+<div class="col-lg-4 trigger">
+</div>
+</div>
+<!-- end row -->
+</div>
+<!-- end container -->
+</section>
+<?php elseif ( get_row_layout() =="faq_left_title"): ?>
+<section class="content-section">
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-6">
+<div class="section-title text-left">
+<h2><?php the_sub_field('faq_title'); ?></h2>
+</div>
+<!-- end section-title -->
+</div>
+<!-- end col-6 -->
+<div class="col-lg-6">
+<?php if (have_rows ("faq")): while(have_rows("faq")): the_row(); ?>
+<dl class="accordion">
+<dt><a href="#"> <?php the_sub_field("question"); ?></a></dt>
+<dd> <?php the_sub_field("answer"); ?></dd>
+</dl>
+<!-- end accordion -->
+<?php endwhile; endif; ?>
+</div>
+<!-- end col-6 -->
+</div>
+<!-- end row -->
+</div>
+<!-- end container -->
+</section>
 
-if( $image ):
+<!-- end content-section -->
+<div class="clearfix spacing-100"></div>
 
-    // Image variables.
-    $url = $image['url'];
-    $title = $image['title'];
-    $alt = $image['alt'];
-    $caption = $image['caption'];
-
-    // Thumbnail size attributes.
-    $size = 'thumbnail';
-echo $url;?>
-	  <img src="<?php echo esc_url($url); ?>" alt="<?php echo esc_attr($alt); ?>" />
-	  <?php
- endif; ?>
-	  
-<p>
-	Gallery starts
-	  </p>
-<?php 
-
-$images = get_field('upload_submission_image_gallery');
-$size = 'uncropped'; // (thumbnail, medium, large, full or custom size)
-if( $images ):
-    ?>
-  <ul class="image-gallery">
-    <?php foreach( $images as $image_id ): ?>
-      <li>
-      
-        <a href="???"  data-lightbox="gal[0]">
-        <?php echo wp_get_attachment_image_url( $image_id, $size ); //use the wp_get_attachment_image_url instead ?>
-        </a>
-      </li>
-    <?php endforeach; ?>
-  </ul>
-<?php endif; ?>
+<?php endif; endwhile; ?>
