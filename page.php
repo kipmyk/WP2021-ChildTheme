61fc8aa0f72b90d8c40f1f9c6cd55961e3b3987a
kipmyk <kipmyk@gmail.com>
2021-09-03 13:19:56 +0300

    page.php updated

--- page.php
+++ page.php
@@ -16,3 +16,26 @@ get_header();
 <main id="site-content" role="main">
   <div class="entry-content" style="margin: 200px !important;">
 
+<?php 
+	  
+$featured_posts = get_field('featured_post');
+if( $featured_posts ): ?>
+    <ul>
+    <?php foreach( $featured_posts as $post ): 
+
+        // Setup this post for WP functions (variable must be named $post).
+        setup_postdata($post); ?>
+		<pre>
+		<?php //print_r($post); ?>
+		</pre>
+        <li>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            <span>A custom field from this post: <?php the_field( 'field_name' ); ?></span>
+			<?php the_author() ?>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+    <?php 
+    // Reset the global post object so that the rest of the page works correctly.
+    wp_reset_postdata(); ?>
+<?php endif; ?>
