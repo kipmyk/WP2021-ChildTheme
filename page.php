7007f7378da4054de8f1c126bca7eda074a4da6f
kipmyk <kipmyk@gmail.com>
2021-09-07 13:07:30 +0300

    pages

--- page.php
+++ page.php
@@ -17,25 +17,8 @@ get_header();
   <div class="entry-content" style="margin: 200px !important;">
 
 <?php 
-	  
-$featured_posts = get_field('featured_post');
-if( $featured_posts ): ?>
-    <ul>
-    <?php foreach( $featured_posts as $post ): 
-
-        // Setup this post for WP functions (variable must be named $post).
-        setup_postdata($post); ?>
-		<pre>
-		<?php //print_r($post); ?>
-		</pre>
-        <li>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            <span>A custom field from this post: <?php the_field( 'field_name' ); ?></span>
-			<?php the_author() ?>
-        </li>
-    <?php endforeach; ?>
-    </ul>
-    <?php 
-    // Reset the global post object so that the rest of the page works correctly.
-    wp_reset_postdata(); ?>
+$term = get_field('taxonomy_field_name');
+if( $term ): ?>
+    <h2><?php echo esc_html( $term->name ); ?></h2>
+    <p><?php echo esc_html( $term->description ); ?></p>
 <?php endif; ?>
