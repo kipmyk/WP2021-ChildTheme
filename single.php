321a72bd71fc4a692c7dd0815dbbc5d08e22d28a
kipmyk <kipmyk@gmail.com>
2021-09-07 13:07:50 +0300

    single posts

--- single.php
+++ single.php
@@ -16,93 +16,31 @@ get_header();
 
 <main id="site-content" role="main">
   <div class="entry-content" style="margin: 200px !important;">
+<?php
 
-<?php while( the_flexible_field("flexible_content") ): ?>
-<?php if( get_row_layout() == "full_width_paragraph" ): ?>
-<section class="content-section">
-<div class="container">
-<div class="row">
-<div class="col-lg-12">
-<div class="text-box">
-<h2 class="heading-genekor"><?php the_sub_field("paragraph_title"); ?></h2>
-<p class="genekor"><?php the_sub_field("paragraph_content"); ?></p>
-<div class="clearfix spacing-50"></div>
-<p class="genekor"><?php the_sub_field("subtitle"); ?></p>
-<ul id="list">
-<?php if(have_rows ('list')): while (have_rows ('list')): the_row(); ?>
-<li><?php the_sub_field("list_item"); ?></li>
-<?php endwhile; endif; ?>
-</ul>
-</div>
-<div class="clearfix spacing-50"></div>
-</div>
-<!-- end col-12 -->
-</div>
-<!-- end row -->
-</div>
-<!-- end container -->
-</section>
+// Check value exists.
+if( have_rows('flexible') ):
 
-<!-- end content-section -->
+    // Loop through rows.
+    while ( have_rows('flexible') ) : the_row();
 
-<?php elseif( get_row_layout() == "left_content_right_image_svg" ): ?>
-<section class="content-section" data-background="<?php the_sub_field('background_color'); ?>">
-<div class="container">
-<div class="row align-items-center">
-<div class="col-lg-8">
-<div class="side-content left">
-<h2 class="heading-genekor"><?php the_sub_field('title'); ?></h2>
-<p class="genekor-text" style="font-size:17px;">
-<?php the_sub_field('content'); ?>
-</p>
-<div class="clearfix spacing-50"></div>
-</div>
-<!-- end side-content -->
-<div class="clearfix spacing-50"></div>
-</div>
-<!-- end col-8 -->
-<div class="col-lg-4 trigger">
-</div>
-</div>
-<!-- end row -->
-</div>
-<!-- end container -->
-</section>
-<?php elseif ( get_row_layout() =="faq_left_title"): ?>
-<section class="content-section">
-<div class="container">
-<div class="row justify-content-center">
-<div class="col-6">
-<div class="section-title text-left">
-<h2><?php the_sub_field('faq_title'); ?></h2>
-</div>
-<!-- end section-title -->
-</div>
-<!-- end col-6 -->
-<div class="col-lg-6">
-<?php if (have_rows ("faq")): while(have_rows("faq")): the_row(); ?>
-<dl class="accordion">
-<dt><a href="#"> <?php the_sub_field("question"); ?></a></dt>
-<dd> <?php the_sub_field("answer"); ?></dd>
-</dl>
-<!-- end accordion -->
-<?php endwhile; endif; ?>
-</div>
-<!-- end col-6 -->
-</div>
-<!-- end row -->
-</div>
-<!-- end container -->
-</section>
+        // Case: Paragraph layout.
+        if( get_row_layout() == 'paragraph' ):
+            $text = get_sub_field('text');
+            echo $text;
+            // Do something...
 
-<!-- end content-section -->
-<div class="clearfix spacing-100"></div>
+        // Case: Download layout.
+        elseif( get_row_layout() == 'download' ): 
+            $file = get_sub_field('file');
+            // Do something...
 
-<?php endif; endwhile; 
-	  
-	  $fields = get_field_object( 21805 );
-	  echo '<pre>';
-	  print_r($fields);
-	  echo '</pre>';
-	  
-	  
+        endif;
+
+    // End loop.
+    endwhile;
+
+// No value.
+else :
+    // Do something...
+endif;
