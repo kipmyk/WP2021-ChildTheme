8253573bdde6dec744731177c5dbcf2622794cad
kipmyk <kipmyk@gmail.com>
2021-09-10 07:01:50 +0300

    testing

--- templates/template.php
+++ templates/template.php
@@ -1,16 +1,26 @@
 <?php
 /**
- * Template Name: Page Tempalate Name
+ * Template Name: Front Form Template
  * Template Post Type: post, page
  *
  * @package WordPress
  * @subpackage Twenty_Twenty
  * @since Twenty Twenty 1.0
  */
+ ?>
 
-get_header();
-?>
 
+
+
+<?php acf_form_head(); ?>
+<?php get_header(); ?>
 <main id="site-content" role="main">
   <div class="entry-content" style="margin: 200px !important;">
 
+<div id="primary" class="content-area">
+    <div id="content">
+        <?php acf_form('new-event'); ?>
+    </div><!-- #content -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
